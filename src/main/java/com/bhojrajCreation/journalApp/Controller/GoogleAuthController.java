e94a26554da17b7a8d926e4eb80719dbe8276a49
bhojrajgurjar <bgurjar2000@gmail.com>
2025-09-26 14:14:47 +0530

    auth2 remove

--- src/main/java/com/bhojrajCreation/journalApp/Controller/GoogleAuthController.java
+++ src/main/java/com/bhojrajCreation/journalApp/Controller/GoogleAuthController.java
@@ -1,12 +1,7 @@
 package com.bhojrajCreation.journalApp.Controller;
 
-import com.bhojrajCreation.journalApp.Services.GoogleAuthService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,12 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class GoogleAuthController {
 
 
-    @Autowired
-    private GoogleAuthService googleAuthService;
 
-    @GetMapping("/callback")
-    public ResponseEntity<?> handleGoogleCallback(@RequestParam String code){
 
-        return googleAuthService.googleCallback(code);
-    }
+//    @GetMapping("/callback")
+//    public ResponseEntity<?> handleGoogleCallback(@RequestParam String code){
+//
+//        return googleAuthService.googleCallback(code);
+//    }
 }
